844a9e9dc29f0b9d22e6a7e51d6db880e73150c8
eduard-castro <117235584+eduard-castro@users.noreply.github.com>
2022-11-04 11:59:24 -0400

    Add files via upload

---
+++ Estudiante20191182/Data/Estudiante.cs
@@ -0,0 +1,9 @@
+namespace Estudiante20191182.Data.Models;
+public class estudiante {
+    public int estudianteID {get;set;}
+    public string? Nombre {get;set;}
+   public string? Apellido {get;set;}
+   public string? FechaNacimiento {get;set;}
+public string? Matricula {get;set;}
+public string? Carrera {get;set;}
+}
